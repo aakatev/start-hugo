d67a539c2ea871d3e9a8f593b0ec39e66f0d0268
aakatev <aakatev@mail.sfsu.edu>
2020-10-12 15:16:27 -0700

    Add function to handle zips

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,38 @@
-exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
-  return {
+const Archiver = require('archiver')
+
+const formatResponse = function (body) {
+  var response = {
     statusCode: 200,
-    body: `Hello ${subject}!`,
+    headers: {
+      'Content-Type': 'application/zip',
+      'Content-disposition': `attachment; filename=site${Date.now()}.zip`,
+    },
+    isBase64Encoded: true,
+    body: body.toString('base64'),
   }
+  return response
+}
+
+exports.handler = async (event) => {
+  return (promise = new Promise((resolve, reject) => {
+    let zip = Archiver('zip')
+    zip
+      .append(
+        '<!doctype html><html lang="en"> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> <title>Hello, world!</title> </head> <body> <h1>Hello, world!</h1> <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script> <script src="./scripts/index.js"></script> </body></html>',
+        { name: 'index.html' }
+      )
+      .append('console.log("Hello, wordl!")', {
+        name: 'scripts/index.js',
+      })
+      .finalize()
+
+    let buffer = []
+
+    zip.on('data', (data) => buffer.push(data))
+
+    zip.on('end', () => {
+      let data = Buffer.concat(buffer)
+      resolve(formatResponse(data))
+    })
+  }))
 }
