21d4a9a1a4fb2999cf18e7f7a32c68655551f4ba
aakatev <aakatev@mail.sfsu.edu>
2020-10-12 16:37:42 -0700

    Generate boilerplate functions added

--- functions/index.js
+++ functions/index.js
@@ -13,17 +13,159 @@ const formatResponse = function (body) {
   return response
 }
 
+const configFile = `baseURL = "/"
+languageCode = "en-us"
+title = "Hugo Starter Website"
+publishDir = "docs"
+paginate = 3`
+
+const baseOfFile = `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS pipe -->
+    {{ $style := resources.Get "styles/main.css" | resources.Minify | resources.Fingerprint }}
+    <link rel="stylesheet" href="{{ $style.Permalink }}">
+    <!-- JavaScript pipe -->
+    {{ $built := resources.Get "js/index.js" | js.Build "bundle.js" }}
+    <script type="text/javascript" src="{{ $built.RelPermalink }}" defer></script>
+    <title>{{ .Site.Title }}</title>
+  </head>
+  <body>
+    {{ block "main" . }}
+    {{ end }}
+  </body>
+</html>`
+
+const singleFile = `{{ define "main" }}
+  <div class="layout">
+    <div class="content">{{ .Content }}</div>
+  </div>
+{{ end }}`
+
+const listFile = `{{ define "main" }}
+  <div class="layout">
+    {{ range .Paginator.Pages }}
+      <div class="page">
+        <h3>{{.Title}}</h3>
+        <a href="{{.Permalink}}">learn more</a>
+      </div>
+    {{ end }}
+    {{ template "_internal/pagination.html" . }}
+  </div>
+{{ end }}`
+
+const stylesFile = `html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}`
+
+const scriptFile = `console.log("Hugo is awesome!!! 🚀")`
+
+const createPostFile = (title, body) => `---
+title: '${title}'
+draft: false
+---
+${body}`
+
+const archetypeFile = `---
+title: "{{ replace .Name "-" " " | title }}"
+date: {{ .Date }}
+draft: true
+---`
+
 exports.handler = async (event) => {
   return (promise = new Promise((resolve, reject) => {
     let zip = Archiver('zip')
     zip
       .append(
-        '<!doctype html><html lang="en"> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> <title>Hello, world!</title> </head> <body> <h1>Hello, world!</h1> <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script> <script src="./scripts/index.js"></script> </body></html>',
-        { name: 'index.html' }
+        configFile,
+        { name: 'config.toml' }
       )
-      .append('console.log("Hello, wordl!")', {
-        name: 'scripts/index.js',
-      })
+      .append(
+        baseOfFile,
+        { name: 'layouts/_default/baseof.html' }
+      )
+      .append(
+        singleFile, 
+        { name: 'layouts/_default/single.html'}
+      )
+      .append(
+        listFile, 
+        { name: 'layouts/_default/list.html'}
+      )
+      .append(
+        stylesFile, 
+        { name: 'assets/styles/main.css'}
+      )
+      .append(
+        scriptFile, 
+        { name: 'assets/js/index.js'}
+      )
+      .append(
+        archetypeFile, 
+        { name: 'archetypes/default.md'}
+      )
+      .append(
+        createPostFile('My First Post', '## Welcome to my first post'), 
+        { name: 'content/posts/my-first-post.md'}
+      )
+      .append(
+        createPostFile('My Other Post', '## Welcome to my other post'), 
+        { name: 'content/posts/my-other-post.md'}
+      )
+      .append(
+        '', 
+        { name: 'data/.gitkeep'}
+      )
+      .append(
+        '', 
+        { name: 'themes/.gitkeep'}
+      )
+      .append(
+        '', 
+        { name: 'static/.gitkeep'}
+      )
       .finalize()
 
     let buffer = []
