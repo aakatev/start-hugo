1aaa625216f7ff09d936e2c2e23f79c2f86327d3
aakatev <aakatev@mail.sfsu.edu>
2020-10-14 19:46:40 -0700

    Theme component added

--- src/components/generator/assets.js
+++ src/components/generator/assets.js
@@ -4,13 +4,14 @@ import Input from '@material-ui/core/Input'
 import InputLabel from '@material-ui/core/InputLabel'
 import FormControl from '@material-ui/core/FormControl'
 import Title from '../title'
-import Select from '@material-ui/core/Select'
-import MenuItem from '@material-ui/core/MenuItem'
 import Grid from '@material-ui/core/Grid'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import Button from '@material-ui/core/Button'
+import Modal from '@material-ui/core/Modal'
+import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -28,13 +29,32 @@ const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(3),
   },
+  modalPaper: {
+    position: 'absolute',
+    width: '80vw',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
 }))
+
+function getModalStyle() {
+  const top = 50
+  const left = 50
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  }
+}
 
 export default function Assets({ assets, setAssets }) {
   const classes = useStyles()
 
   const hanldeRemoveAsset = (item) => {
-    setAssets([...assets.filter(i => i !==item)])
+    setAssets([...assets.filter((i) => i !== item)])
   }
 
   const hanldeUpdateAsset = (index) => (event) => {
@@ -43,33 +63,84 @@ export default function Assets({ assets, setAssets }) {
     setAssets(newAssets)
   }
 
+  const [modalStyle] = React.useState(getModalStyle)
+  const [open, setOpen] = React.useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const body = (
+    <div style={modalStyle} className={classes.modalPaper}>
+      <Title>Add new asset</Title>
+      <Grid xs={12} item>
+        <FormControl fullWidth className={classes.margin} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-name">
+            Link in a format https://example.com/style.css
+          </InputLabel>
+          <Input
+            id="outlined-adornment-styles"
+            value={'url'}
+            onChange={hanldeUpdateAsset()}
+          />
+        </FormControl>
+      </Grid>{' '}
+      <Grid xs={6} item>
+        <Button onClick={handleOpen} color="primary">
+          Add CSS/JS
+        </Button>
+      </Grid>
+    </div>
+  )
+
   return (
     <React.Fragment>
-      <Title>Assets</Title>
-      {assets.map((url, index) => (
-        <Grid xs={12} key={url+index} item>
-          <FormControl fullWidth className={classes.margin} variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-name">
-              Link in a format https://example.com/style.css
-            </InputLabel>
-            <Input
-              id="outlined-adornment-styles"
-              value={url}
-              onChange={hanldeUpdateAsset(index)}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton onClick={() => hanldeRemoveAsset(url)}>
-                    <DeleteIcon />
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-        </Grid>
-      ))}
+      <Title>Assets ({assets.length})</Title>
+      {assets.length === 0
+        ? <Typography>No assets added</Typography>
+        : assets.map((url, index) => (
+            <Grid xs={12} key={url + index} item>
+              <FormControl
+                fullWidth
+                className={classes.margin}
+                variant="outlined"
+              >
+                <Input
+                  disabled
+                  id="outlined-adornment-styles"
+                  value={url}
+                  onChange={hanldeUpdateAsset(index)}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton onClick={() => hanldeRemoveAsset(url)}>
+                        <EditIcon />
+                      </IconButton>
+                      <IconButton onClick={() => hanldeRemoveAsset(url)}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+          ))}
       <Grid xs={6} item>
-        <Button color="primary">Add CSS/JS</Button>
+        <Button onClick={handleOpen} color="primary">
+          Add CSS/JS
+        </Button>
       </Grid>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
     </React.Fragment>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app'
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#0a1922',
+    },
+    secondary: {
+      main: '#33ba91',
+    },
+  },
+})
 const Root = () => (
   <React.Fragment>
-    <CssBaseline />
-    <App/>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.Fragment>
 )
 
